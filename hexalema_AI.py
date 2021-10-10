116e09bc686ff19b5d50495ca9d1c9b848cbc468
Daniel Sykora <daniel.sykora5@gmail.com>
2021-10-10 21:23:54 +0200

    Update hexalema_AI.py

--- hexalema_AI.py
+++ hexalema_AI.py
@@ -1,4 +1,5 @@
 import random
+import copy
 
 #                      1 1 1 1 1
 #  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4
@@ -46,8 +47,6 @@ def kolik_me_ohrozuje(stav,MOJE_KAMENY,NEPRITEL_KAMENY):
     for i in range(len(stav)):
         if NEPRITEL_KAMENY in stav[i]:
             nepritelem_obsazene.append(i)
-
-    print(nepritelem_obsazene)
     
     sance_ze_me_vyhodi = []
     for on in nepritelem_obsazene:
@@ -69,7 +68,99 @@ def vyhodnotit_stav(stav,MOJE_KAMENY,ai_params):
     moje_odevzdane = stav[-1].count(MOJE_KAMENY)
 
     # Cim bliz je k 1, tim lip se mi dari.
-    koeficient_uspechu = min(moje_odevzdane/nepritel_odevzdane,1)
+    koeficient_uspechu = min((moje_odevzdane+1)/(nepritel_odevzdane+1),1)
 
     # Pokud se mi dari, nechci prijit o kameny, a chci bit bliz k cili.
     # Pokud se mi nedari, chci spis ohrozovat nepritele.
+
+    vliv = kolik_ohrozuji(stav,MOJE_KAMENY,NEPRITEL_KAMENY)
+    hrozby = kolik_me_ohrozuje(stav,MOJE_KAMENY,NEPRITEL_KAMENY)
+    kvocna = stav[5].count(MOJE_KAMENY) + stav[9].count(MOJE_KAMENY)
+    zabezpecene_kameny = sum([stav[i].count(MOJE_KAMENY) for i in range(11,14)])
+
+    return 0
+
+def muzu_tahnout(stav,od,o_kolik,MOJE_KAMENY):
+    NEPRITEL_KAMENY = {"A":"B","B":"A"}[MOJE_KAMENY]
+    if od + o_kolik > 14: return False # Pokud bych skoncil mimo herni desku, tak nemuzu
+    if od + o_kolik == 14: return 2 # Vyvadet muzu vzdy
+    if len(stav[od+o_kolik]) == 0: return 3 # Pokud na poli nic neni, tak muzu
+    if len(stav[od+o_kolik]) == 1:
+        if stav[od+o_kolik][0] == MOJE_KAMENY: return False # Pokud bych si stoupl na svuj vlastni kamen, tak ne
+        elif od+o_kolik in [5,9]: return False # Kamen na kvocne nemuzu vyhodit
+        elif od+o_kolik > 3 and od+o_kolik < 11: return 1 # Vyhozeni kamene
+        else: return 3 # Pokud na poli nic neni, tak muzu
+    
+
+def hra():
+    turn = random.choice(["A","B"])
+    gameBoard = [["A","B"] * 7,[],[],[],[],[],[],[],[],[],[],[],[],[],[]]
+    done = False
+    while not done:
+        k = hod_kostkama() # Napred hodim kostkou...
+        if k == 0: # Ztrata tahu.
+            turn = {"A":"B","B":"A"}[turn]
+            continue
+
+        moje_pole = [] # Kde vsude mam kameny?
+        for i in range(len(gameBoard)):
+            if turn in gameBoard[i]:
+                moje_pole.append(i)
+
+        mozne_tahy = [(pole,muzu_tahnout(gameBoard,pole,k,turn)) for pole in moje_pole]
+        mozne_tahy = [tah for tah in mozne_tahy if tah[1]]
+
+        if len(mozne_tahy) == 0: # Ztrata tahu.
+            turn = {"A":"B","B":"A"}[turn]
+            continue
+
+        puvodni_stav = vyhodnotit_stav(gameBoard,turn,[])
+        nejlepsi_skore = -999999
+        nejlepsi_tah = None
+        for tah in mozne_tahy:
+            tempGameBoard = copy.deepcopy(gameBoard)
+            pole = tah[0]
+            typ = tah[1]
+            odkud = tempGameBoard[pole]
+            kam = tempGameBoard[pole+k]
+            if typ == 1: # Pokud dochazi k vyhazovani...
+                tempGameBoard[0].append(kam.pop(0)) # Kamen na destinaci vrat na zacatek
+                odkud.remove(turn) # Seber jeden muj kamen
+                kam.append(turn) # Pridej tam jeden muj kamen
+            else:
+                odkud.remove(turn) # Seber jeden muj kamen
+                kam.append(turn) # Pridej tam jeden muj kamen
+
+            s = vyhodnotit_stav(tempGameBoard,turn,[]) - puvodni_stav
+            if s > nejlepsi_skore:
+                nejlepsi_skore = s
+                nejlepsi_tah = tah
+
+        pole = nejlepsi_tah[0]
+        typ = nejlepsi_tah[1]
+        odkud = gameBoard[pole] # Pozice ze ktere tahnu
+        kam = gameBoard[pole+k]
+        if typ == 1: # Pokud dochazi k vyhazovani...
+            gameBoard[0].append(kam.pop(0)) # Kamen na destinaci vrat na zacatek
+            odkud.remove(turn) # Seber jeden muj kamen
+            kam.append(turn) # Pridej tam jeden muj kamen
+        else:
+            odkud.remove(turn) # Seber jeden muj kamen
+            kam.append(turn) # Pridej tam jeden muj kamen
+
+        if pole+k in [5,9]:
+            turn = {"A":"B","B":"A"}[turn]
+        turn = {"A":"B","B":"A"}[turn]
+
+        if gameBoard[-1].count("A") == 7:
+            done = True
+            print("Vyhrava A")
+        elif gameBoard[-1].count("B") == 7:
+            done = True
+            print("Vyhrava B")
+
+    print("Hra skoncila.")
+                
+            
+        
+    
